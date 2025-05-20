83521854d790bcb635be9fe2cae9798784bf8b7c
anna-tomilova <65096857+anna-tomilova@users.noreply.github.com>
2025-05-20 20:01:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import asyncio
-import os
 from aiogram import Bot, Dispatcher
 from bot.handlers import spin
 from bot.database import init_db
 from bot.config_reader import config
 
 async def main():
+    # Инициализируем бота и диспетчер
     bot = Bot(token=config.bot_token, parse_mode="HTML")
-    port = int(os.environ.get("PORT", 5000))  # Default fallback port 5000
-    bot.run(host="0.0.0.0", port=port)
     dp = Dispatcher()
+
+    # Подключаем роутеры
     dp.include_router(spin.router)
+
+    # Инициализация базы данных
     await init_db()
+
+    # Запуск бота в режиме long polling
     await dp.start_polling(bot)
+
 if __name__ == "__main__":
     asyncio.run(main())
