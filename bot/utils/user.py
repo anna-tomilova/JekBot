b4927ef7d70ef5f00d988870587173e572220d64
anna-tomilova <65096857+anna-tomilova@users.noreply.github.com>
2025-05-21 14:26:35 +0800

    Update user.py

--- bot/utils/user.py
+++ bot/utils/user.py
@@ -4,12 +4,9 @@ from bot.models import User
 async def get_or_create_user(user_id: int, session) -> User:
     result = await session.execute(select(User).where(User.user_id == user_id))
     user = result.scalar_one_or_none()
-    
-    if user:
-        return user
-
-    user = User(user_id=user_id, score=1000)  # начальный баланс
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
+    if user is None:
+        user = User(user_id=user_id, score=1000)
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
     return user
