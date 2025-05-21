91e7566c7ada71e6be415ecf7c423cac9d6536d0
anna-tomilova <65096857+anna-tomilova@users.noreply.github.com>
2025-05-21 13:56:06 +0800

    Update user.py

--- bot/utils/user.py
+++ bot/utils/user.py
@@ -1,16 +1,14 @@
 from sqlalchemy import select
-from bot.database import SessionLocal
 from bot.models import User
 
 async def get_or_create_user(user_id: int, session) -> User:
     result = await session.execute(select(User).where(User.user_id == user_id))
     user = result.scalar_one_or_none()
-
+    
     if user:
         return user
 
-    # Создаем нового пользователя, если не найден
-    user = User(user_id=user_id)
+    user = User(user_id=user_id, score=1000)  # начальный баланс
     session.add(user)
     await session.commit()
     await session.refresh(user)
